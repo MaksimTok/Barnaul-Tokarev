6a7aea3c4b25c95515b3bed8a0419b826a8edec3
maksi <uchebmaksimtok2006@yandex.ru>
2022-11-24 21:01:11 +0700

    Готово

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gitlesson_2.ui', self)
+        self.btn_draw.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(*self.random_coords())
+        qp.end()
+
+    def random_coords(self):
+        x = random.randint(10, 215)
+        y = random.randint(10, 215)
+        dur = random.randint(0, 50)
+        return x, y, x + dur, x + dur
+
+    def paint(self):
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
